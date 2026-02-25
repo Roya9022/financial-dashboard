99fcac001d4ed51175dc0acc1b5b3471cdcf146c
Roya90 <r_azemi@yahoo.com>
2026-02-25 18:53:44 -0500

    chore: default theme dark

--- src/store/useThemeStore.ts
+++ src/store/useThemeStore.ts
@@ -9,7 +9,7 @@ interface ThemeState {
 export const useThemeStore = create<ThemeState>()(
   persist(
     (set) => ({
-      isDarkMode: false,
+      isDarkMode: true,
       toggleTheme: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
     }),
     { name: 'theme-storage' },
